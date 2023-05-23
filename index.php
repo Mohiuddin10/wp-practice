53f92ab055f28203ccf3fca8c60742888f15edb1
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-05-24 01:01:16 +0600

    style added

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
     <?php wp_head(  ); ?>
 </head>
 <body <?php body_class( ); ?>>
+<div id="header_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 

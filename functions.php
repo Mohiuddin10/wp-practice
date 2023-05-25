bf67147006a8ddcdb333724b91705ba76d66ca9c
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-05-25 09:28:29 +0600

    header update

--- functions.php
+++ functions.php
@@ -18,3 +18,23 @@ function mh_css_js_file_calling() {
     wp_enqueue_script( 'main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts','mh_css_js_file_calling' );
+
+
+//Theme function
+function mh_customizar_register ($wp_customize) {
+    $wp_customize->add_section('mh_header_area', array(
+        'title'=>__('Header Area', 'mohammadmohiuddin'),
+        'description'=>'If you want to update your header area, you may change it from here'
+    ));
+    $wp_customize->add_setting('mh_logo', array(
+        'default'=>get_bloginfo( 'template_directory' ). '/img/logo.png',
+    ));
+    $wp_customize-> add_control(new WP_Customize_Image_Control($wp_customize, 'mh_logo', array(
+        'label'=>'Logo Upload',
+        'description'=> 'If you want to upload logo, please go ahead',
+        'setting'=>'mh_logo',
+        'section'=>'mh_header_area',
+    )));
+}
+
+add_action( 'customize_register','mh_customizar_register' );
